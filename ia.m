0ad5c2bf1f5bdd0c8fadfd34c704b99dcfe82d78
abr-evangelista <gabrielgoncalves@estudante.ufscar.br>
2023-08-02 19:06:16 -0300

    Manipulação de folder adicionada
    Uma pequena parte no inicio do script modificada para manipular varios arquivos dentro de uma pasta. Impressão dos valores no Shell do Octave confirma o funcionamento. Comentei o código de teste de imagens pra focar na manipulação de folders por enquanto.

--- ia.m
+++ ia.m
@@ -3,61 +3,59 @@ clear all
 clc
 pkg load image
 
+folder_name = readdir("Banco de Imagens")
+
+[lin col]=size(folder_name);
+
+for i = 3:lin
+  printf(folder_name{i});
+endfor
+
+##A = imread("test.jpg");
 ##
-##folder_name = readdir("Banco de Imagens");
+##figure, imhist(A);
 ##
-##folder_name
+##A = rgb2gray(A);
 ##
-##for i = 3:
-##  printf("%d", i);
-##endfor
+##A = ~im2bw(A, 0.5);
 ##
-
-A = imread("test.jpg");
-
-figure, imhist(A);
-
-A = rgb2gray(A);
-
-A = ~im2bw(A, 0.5);
-
-figure, imshow(A);
-
-A2 = bwmorph(A, 'thin', Inf);
-
-figure, imshow(A2);
-
-B = imread("test2.jpg");
-
-figure, imhist(B);
-
-B = rgb2gray(B);
-
-figure, imhist(B);
-
-B = ~im2bw(B, 0.5);
-
-figure, imshow(B);
-
-B2 = bwmorph(B, 'thin', Inf);
-
-figure, imshow(B2);
-
-C = imread("test3.jpg");
-
-figure, imhist(C);
-
-C = rgb2gray(C);
-
-figure, imshow(C);
-
-C = ~im2bw(C, 0.2);
-
-figure, imshow(C);
-
-C2 = bwmorph(C, 'thin', Inf);
-
-figure, imshow(C2);
+##figure, imshow(A);
+##
+##A2 = bwmorph(A, 'thin', Inf);
+##
+##figure, imshow(A2);
+##
+##B = imread("test2.jpg");
+##
+##figure, imhist(B);
+##
+##B = rgb2gray(B);
+##
+##figure, imhist(B);
+##
+##B = ~im2bw(B, 0.5);
+##
+##figure, imshow(B);
+##
+##B2 = bwmorph(B, 'thin', Inf);
+##
+##figure, imshow(B2);
+##
+##C = imread("test3.jpg");
+##
+##figure, imhist(C);
+##
+##C = rgb2gray(C);
+##
+##figure, imshow(C);
+##
+##C = ~im2bw(C, 0.2);
+##
+##figure, imshow(C);
+##
+##C2 = bwmorph(C, 'thin', Inf);
+##
+##figure, imshow(C2);
 
 
 
