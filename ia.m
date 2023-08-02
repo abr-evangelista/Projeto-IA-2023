0afd1c6b8993a389e147e2836b4c9112bf5766f3
abr-evangelista <gabrielgoncalves@estudante.ufscar.br>
2023-08-02 18:47:44 -0300

    Criação de ia.m
    Script inicial para tratamento de imagens adicionado. Muito a ser mudado ainda, especificamente a automatização pra imagens dentro de um folder e o tratamento em sí de cada imagem.

---
+++ ia.m
@@ -0,0 +1,63 @@
+close all
+clear all
+clc
+pkg load image
+
+##
+##folder_name = readdir("Banco de Imagens");
+##
+##folder_name
+##
+##for i = 3:
+##  printf("%d", i);
+##endfor
+##
+
+A = imread("test.jpg");
+
+figure, imhist(A);
+
+A = rgb2gray(A);
+
+A = ~im2bw(A, 0.5);
+
+figure, imshow(A);
+
+A2 = bwmorph(A, 'thin', Inf);
+
+figure, imshow(A2);
+
+B = imread("test2.jpg");
+
+figure, imhist(B);
+
+B = rgb2gray(B);
+
+figure, imhist(B);
+
+B = ~im2bw(B, 0.5);
+
+figure, imshow(B);
+
+B2 = bwmorph(B, 'thin', Inf);
+
+figure, imshow(B2);
+
+C = imread("test3.jpg");
+
+figure, imhist(C);
+
+C = rgb2gray(C);
+
+figure, imshow(C);
+
+C = ~im2bw(C, 0.2);
+
+figure, imshow(C);
+
+C2 = bwmorph(C, 'thin', Inf);
+
+figure, imshow(C2);
+
+
+
