1d2ca8813e6ee4392ab40593d7e872e7194f586e
Noxilas <nicholaspapadopoulos89@estudante.ufscar.br>
2023-09-03 14:59:33 -0300

    varrer Atualizado (salva a imagem com menos pontos em um arquivo json)

--- varrerContornos.py
+++ varrerContornos.py
@@ -4,14 +4,50 @@ import json
 
 # Create a dictionary to store contours for each image
 
-for j in range(1, 3):
+def scale_contour(cnt, scale):
+    M = cv2.moments(cnt)
+    if M["m00"] != 0:
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+    else:
+        # set values as what you need in the situation
+        cx, cy = 0, 0
+
+    cnt_norm = cnt - [cx, cy]
+    cnt_scaled = cnt_norm * scale
+    cnt_scaled = cnt_scaled + [cx, cy]
+    cnt_scaled = cnt_scaled.astype(np.int32)
+
+    return cnt_scaled 
+
+fixed_contour_length = 100
+
+for j in range(1, 2):
     # Load an image
     image_path = f'./banco_destino/image{j}.png'
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Find contours in the grayscale image
-    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    im_contours_dict = {
+        "contornos": []
+    }
+
+    preprocessed_contours = []
+    for contour in contours:
+        leng = len(contour)
+        if leng < fixed_contour_length:
+            contour = cv2.approxPolyDP(contour, 0.02, closed=True)
+            if leng < fixed_contour_length:
+                contour = scale_contour(contour, 1.1)
+        elif leng > fixed_contour_length:
+            contour = cv2.approxPolyDP(contour, fixed_contour_length, closed=True)
+        preprocessed_contours.append(contour)
+        im_contours_dict["contornos"].append(contour.tolist())
+    
+
 
     # Define minimum and maximum area thresholds for filtering contours
     min_contour_area = 7  # Adjust this value as needed
@@ -20,10 +56,13 @@ for j in range(1, 3):
     # Merge near contours and filter noisy contours
     contours_filtered = []
 
-    for contour in contours:
+    for contour in preprocessed_contours:
         area = cv2.contourArea(contour)
         if min_contour_area <= area <= max_contour_area:
             contours_filtered.append(contour)
+
+    
+    
 
     # Create dictionaries to store contours for each category
     contours_dict = {
@@ -57,8 +96,8 @@ for j in range(1, 3):
 
     cv2.destroyAllWindows()
 
+    with open(f'banco_destino_json/image{j}.json', 'w') as json_file:
+        json.dump(im_contours_dict, json_file, indent=2)
     # Salva separadamente o json de labelling
     with open(f'label/image{j}.json', 'w') as json_file:
-        json.dump(contours_dict, json_file, indent=2)  
-
-
+        json.dump(contours_dict, json_file, indent=2) 
